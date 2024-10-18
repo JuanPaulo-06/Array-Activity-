f9d99fa00c7232e87b375569fdd5d715119d310d
Juan Paulo Passi <passipaulo14@gmail.com>
2024-10-18 09:55:29 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,9 @@
+let favSingers = ["Bruno Mars", "Billie Eilish", "Justin Bieber"];
+ console.log(favSingers[0]);
+
+
+let favNumbers = [6, 15, 23];
+console.log(favNumbers);
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr);
